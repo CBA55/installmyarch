61e54d4ac9fd672df77c9f95ba9d223e270857d6
CBA55 <inforenderproject@gmeil.com>
2022-05-10 12:24:50 -0300

    update

--- install-my-arch.sh
+++ install-my-arch.sh
@@ -1,13 +1,10 @@
 #!/bin/bash +x
 # Unnatended Archlinux Installer - by Sebastian Sanchez Baldoncini
 # -------------------------------
-# Store log file
-SYSERR=/tmp/install-my-arch.sh.$$
-
 # Store menu options selected by the user
 INPUT=/tmp/menu.sh.$$
 
-# Storage file for command outputs display
+# Storage files with display info
 OUTPUT=/tmp/output.sh.$$
 PARTS=/tmp/parts.sh.$$
 LAYOUTS=/tmp/layouts.sh.$$
@@ -360,7 +357,7 @@ fi
 
 #------------------[ CHECK DIRS AND MOUNT ]---------------------
 
-clockfor="\n[*] Mounting partitions\n "
+clockfor="[*] Mounting partitions"
 reverse_clock
 
 # Create and mount root
@@ -380,7 +377,10 @@ fi
 
 #------------------[ PACSTRAP AND FSTAB ]---------------------
 
-clockfor="\n[*] Start Base Install\n"
+# Store log file
+exec > $RPOINT/log/install_log.sh.$$ 2>&1
+
+clockfor="[*] Start Base Install"
 reverse_clock
 pacstrap $RPOINT $BASE 
 
@@ -479,6 +479,8 @@ if [[ -n $USR1 ]]; then
   $CHR "chown $USR1:users /paru;cd /paru;sudo -u $USR1 makepkg --noconfirm -sci"
   $CHR "rm -rf /paru"
   PARUINSTALL="sudo -u $USR1 paru --noconfirm --color always -S"
+  # Allow PARU without pass (temporary)
+  $CHR "echo -e "%wheel ALL=(ALL) NOPASSWD: /usr/bin/paru" >>/etc/sudoers"
   # OH-MY-ZSHELL + POWERLEVEL10K
   text g "\n[+] Installing and configure oh-my-zshell + powerlevel10k theme\n"
   $CHR "sudo -u $USR1 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)""
@@ -518,51 +520,51 @@ fi
 
 # Setup keyboard layout
 text g "\n[+] Keyboard: Setting latam layout\n"
-echo -e "\n[Layout]\nLayoutList=latam\nModel=acer_laptop\nUse=true" >>/home/$USR1/.config/kxkbrc
+$CHR "echo -e "\n[Layout]\nLayoutList=latam\nModel=acer_laptop\nUse=true" >>/home/$USR1/.config/kxkbrc"
 
 # Disable splash
 text g "\n[+] Diabling ksplash\n"
-echo -e "[KSplash]\nEngine=none\nTheme=None" >/home/$USR1/.config/ksplashrc
+$CHR "echo -e "[KSplash]\nEngine=none\nTheme=None" >/home/$USR1/.config/ksplashrc"
 
 # Disable screen locker
 text g "\n[+] Disabling screen locker\n"
-echo -e "\n[Daemon]\nAutolock=false\nLockOnResume=false" >>/home/$USR1/.config/kscreenlockerrc
+$CHR "echo -e "\n[Daemon]\nAutolock=false\nLockOnResume=false" >>/home/$USR1/.config/kscreenlockerrc"
 
 # Setup powerdevil settings
 text g "\n[+] Configuring energy options\n"
 powerfile="/home/$USR1/.config/powermanagementprofilesrc"
 ## Change values
-sed -i '12 s/1/0/g' $powerfile
-sed -i '13 s/16/8/g' $powerfile
-sed -i '28 s/16/8/g' $powerfile
-sed -i '20 s/300/600/g' $powerfile
-sed -i '24 s/120000/300000/g' $powerfile
-sed -i '13 s/16/8/g' $powerfile
+$CHR "sed -i '12 s/1/0/g' $powerfile"
+$CHR "sed -i '13 s/16/8/g' $powerfile"
+$CHR "sed -i '28 s/16/8/g' $powerfile"
+$CHR "sed -i '20 s/300/600/g' $powerfile"
+$CHR "sed -i '24 s/120000/300000/g' $powerfile"
+$CHR "sed -i '13 s/16/8/g' $powerfile"
 ## Delete lines
-sed -i '3d;4d;5d;6d;7d;8d;9d;30d;31d;32d;33d' $powerfile
+$CHR "sed -i '3d;4d;5d;6d;7d;8d;9d;30d;31d;32d;33d' $powerfile"
 ## Add lines
-sed -i '4 i [AC][BrightnessControl]' $powerfile
-sed -i '5 i value=100' $powerfile
-sed -i '8 i triggerLidActionWhenExternalMonitorPresent=true' $powerfile
-sed -i '24 i triggerLidActionWhenExternalMonitorPresent=false' $powerfile
-sed -i '43 i triggerLidActionWhenExternalMonitorPresent=false' $powerfile
-sed -i '47 i suspendThenHibernate=false' $powerfile
+$CHR "sed -i '4 i [AC][BrightnessControl]' $powerfile"
+$CHR "sed -i '5 i value=100' $powerfile"
+$CHR "sed -i '8 i triggerLidActionWhenExternalMonitorPresent=true' $powerfile"
+$CHR "sed -i '24 i triggerLidActionWhenExternalMonitorPresent=false' $powerfile"
+$CHR "sed -i '43 i triggerLidActionWhenExternalMonitorPresent=false' $powerfile"
+$CHR "sed -i '47 i suspendThenHibernate=false' $powerfile"
 
 # Set numlock on startup
 text g "\n[+] Setting numlock on startup\n"
-echo -e "\n[Layout]\n[Keyboard]\nNumLock=0" >>/home/$USR1/.config/kcminputrc
+$CHR "echo -e "\n[Layout]\n[Keyboard]\nNumLock=0" >>/home/$USR1/.config/kcminputrc"
 
 # Setup touchpad buttons
 text g "\n[+] Setting touchpad options\n"
-echo -e "[SYNA7DB5:01 06CB:CD41 Touchpad]\nclickMethodAreas=false\nclickMethodClickfinger=true\ntapToClick=true\n" >/home/$USR1/.config/touchpadxlibinputrc
+$CHR "echo -e "[SYNA7DB5:01 06CB:CD41 Touchpad]\nclickMethodAreas=false\nclickMethodClickfinger=true\ntapToClick=true\n" >/home/$USR1/.config/touchpadxlibinputrc"
 
 # Setup kde-globals
 ## Icons
 text g "\n[+] KDE: Setting papirus icons theme\n"
-echo -e "\n[Icons]\nTheme=Papirus-Dark" >>/home/$USR1/.config/kdeglobals
+$CHR "echo -e "\n[Icons]\nTheme=Papirus-Dark" >>/home/$USR1/.config/kdeglobals"
 ## Dark theme + delete without confikrm + singleclick
 text g "\n[+] KDE: Changing to dark theme and single click\n"
-echo -e "\n[KDE]\nLookAndFeelPackage=org.kde.breezedark.desktop\nShowDeleteCommand=false\nSingleClick=false" >>/home/$USR1/.config/kdeglobals
+$CHR "echo -e "\n[KDE]\nLookAndFeelPackage=org.kde.breezedark.desktop\nShowDeleteCommand=false\nSingleClick=false" >>/home/$USR1/.config/kdeglobals"
 
 #------------------[ UMOUNT AND REBOOT ]---------------------
 
